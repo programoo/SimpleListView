426f16cbfe63c83ee6036b5ba9e74666432bfdae
programoo <mevtwifu@gmail.com>
2013-08-12 14:22:56 +0700

    Revert "Remove 4 country test"
    This reverts commit 5b6a273187d601f261771b9a0e5b764e07ac9eee.

--- src/com/wongsanit/makhos/RestRoomActivity.java
+++ src/com/wongsanit/makhos/RestRoomActivity.java
@@ -23,6 +23,12 @@ public class RestRoomActivity extends ListActivity {
 		
 		String[] players  = new String[] 
 				{ "Leonel Messi",
+					"Zinedine Zidane",
+					"Diego Maradona",
+					"Marco Van Basten",
+					"Ronaldinho",
+					"Steven Gerrard",
+					"Cristiano Ronaldo",
 					"Gareth Bale",
 					"Neymar",
 					"Zlatan Ibrahimovich" };
